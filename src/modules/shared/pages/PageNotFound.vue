9ddb44b0fdfaa7522c5af7e419ae5bc8c027e84c
vantroysanchez <vantroysanchez@gmail.com>
2023-01-03 22:06:24 -0400

    - Estructurando proyecto con esquema de modulos.

---
+++ src/modules/shared/pages/PageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Page not found</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+}
+h1 {
+  margin: 0;
+}
+</style>
